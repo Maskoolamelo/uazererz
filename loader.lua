2ca6ff3f7fa53f272a468ab9a1f2f1df0eeabea6
Maskoolamelo <129920658+Maskoolamelo@users.noreply.github.com>
2025-11-11 13:33:04 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,1 @@
-pcall(function() delfile('49abdfcd1ce9ec893553a573b9348ca3-cache.lua') end)
-
-  local a pcall(function()a=readfile("static_content_130525/initv4.lua")end) if a and #a>2000 then a=loadstring(a) end;
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/main/DontWaketheSpookyBrainrots"))()
